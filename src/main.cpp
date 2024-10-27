e6958a58168a82a380c353ccbbbbfcc3d71f1bde
josep <joseph_chiu@outlook.com>
2024-10-27 19:51:01 +0800

    new code

--- src/main.cpp
+++ src/main.cpp
@@ -1,164 +1,87 @@
-#include <Wire.h>
-#include <Arduino.h>
+#include <esp_now.h>
+#include <WiFi.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 
-constexpr int MPU = 0x68;
-constexpr int RDX = 0x6b;
-constexpr int ACCEL_CONFIG = 0x1c;
-constexpr int GYRO_CONFIG = 0x1b;
+std::array<std::uint8_t, 6> broadcastAddress = {0xfc, 0xe8, 0xc0, 0xa2, 0x4f, 0x90};
+Adafruit_MPU6050 mpu6050;
 
+class ImuData {
+    float accelerationX;
+    float accelerationY;
+    float accelerationZ;
+    char foot;
 
-double accX {};
-double accY {};
-double accZ {};
+    // to calculate velocity over time -> (t1-t0) * (a1+a0) / 2
+public:
+    ImuData(float accelerationX, float accelerationY, float accelerationZ) {
+        this->accelerationX = accelerationX;
+        this->accelerationY = accelerationY;
+        this->accelerationZ = accelerationZ;
+        this->foot = 'L';
+    }
+};
 
-double gyroX {};
-double gyroY {};
-double gyroZ {};
+void callbackFunc(const uint8_t *mac_addr, esp_now_send_status_t status) {
+    Serial.print("Data send status: ");
+    Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Success" : "Failure");
+}
 
-double accAngleX {};
-double accAngleY {};
-
-
-double gyroAngleX {};
-double gyroAngleY {};
-
-double roll {};
-double yaw {};
-double pitch {};
-
-double accErrorX {};
-double accErrorY {};
-
-
-double gyroErrorX {};
-double gyroErrorY {};
-double gyroErrorZ {};
-
-double elapsedTime {};
-std::uint64_t currentTime {};
-std::uint64_t previousTime {};
-
-
-void calibrate_imu_err();
-
-void read_all_imu_accel();
-
-void read_all_imu_gyro();
-
-void setup() {
+void setup(){
     Serial.begin(115200);
-    if (!Wire.begin()) {
-        Serial.println("I2C bus cannot be initialised. Check the IMU connection");
+
+    delay(100);
+
+    if (!mpu6050.begin(0x68)) {
+        Serial.println("Failed to discover MPU6050 sensor");
         return;
     }
-    Wire.beginTransmission(MPU);
-    Wire.write(RDX);
-    Wire.write(0);
-    Wire.endTransmission(true);
+    Serial.println("MPU6050 Found!");
 
-    Wire.beginTransmission(MPU);
-    Wire.write(ACCEL_CONFIG);
-    Wire.write(0x10);
-    Wire.endTransmission(true);
+    mpu6050.setAccelerometerRange(MPU6050_RANGE_8_G);
+    mpu6050.setGyroRange(MPU6050_RANGE_500_DEG);
+    mpu6050.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
-    Wire.beginTransmission(MPU);
-    Wire.write(GYRO_CONFIG);
-    Wire.write(0x10);
-    Wire.endTransmission(true);
-    delay(20);
+    delay(10000);
 
-    calibrate_imu_err();
-    delay(20);
+    Serial.print("Starting WiFi...");
 
-}
-
-void loop() {
-    read_all_imu_accel();
-
-    accAngleX = std::atan(accY / std::sqrt(std::pow(accX, 2) + std::pow(accZ, 2))) * 180 / PI - 0.58;
-    accAngleY = std::atan(-1 * accX / std::sqrt(std::pow(accY, 2) + std::pow(accZ, 2))) * 180 / PI + 1.58;
-
-    previousTime = currentTime;
-    currentTime = millis();
-
-    elapsedTime = static_cast<double>(currentTime - previousTime) / 1000;
-
-    read_all_imu_gyro();
-
-    gyroX += 0.56;
-    gyroY -= 2;
-    gyroZ += 0.79;
-
-    gyroAngleX += gyroX * elapsedTime;
-    gyroAngleY += gyroY * elapsedTime;
-
-    yaw += gyroZ * elapsedTime;
-    roll = 0.96 * gyroAngleX + 0.04 * accAngleX;
-    pitch = 0.96 * gyroAngleY + 0.04 * accAngleY;
-
-    Serial.print("Acceleration: ");
-    Serial.print(accX);
-    Serial.print(", ");
-    Serial.print(accY);
-    Serial.print(", ");
-    Serial.print(accZ);
-    Serial.println();
-
-    Serial.print("Rotation: ");
-    Serial.print(roll);
-    Serial.print("°, ");
-    Serial.print(pitch);
-    Serial.print("°, ");
-    Serial.print(yaw);
-    Serial.println();
-
-}
-
-void read_all_imu_accel() {
-    Wire.beginTransmission(MPU);
-    Wire.write(0x3b);
-    Wire.endTransmission(true);
-    constexpr std::size_t len {6};
-    Wire.requestFrom(static_cast<std::uint8_t>(MPU), len, true);
-    accX = static_cast<double>(Wire.read() << 8 | Wire.read()) / (2 << 14);
-    accY = static_cast<double>(Wire.read() << 8 | Wire.read()) / (2 << 14);
-    accZ = static_cast<double>(Wire.read() << 8 | Wire.read()) / (2 << 14);
-}
-
-void read_all_imu_gyro() {
-    Wire.beginTransmission(MPU);
-    Wire.write(0x43);
-    Wire.endTransmission(true);
-    constexpr std::size_t len {6};
-    Wire.requestFrom(static_cast<std::uint8_t>(MPU), len, true);
-    gyroX = Wire.read() << 8 | Wire.read();
-    gyroY = Wire.read() << 8 | Wire.read();
-    gyroZ = Wire.read() << 8 | Wire.read();
-}
-
-void calibrate_imu_err() {
-    for (int i {}; i < 200; ++i) {
-        read_all_imu_accel();
-
-        accErrorX += ((std::atan(accY / std::sqrt(std::pow(accX, 2) + std::pow(accZ, 2))) * 180 / PI));
-        accErrorY += ((std::atan(-1 * accY / std::sqrt(std::pow(accY, 2) + std::pow(accZ, 2))) * 180 / PI));
-
+    WiFiClass::mode(WIFI_STA);
+    if (esp_now_init() != ESP_OK) {
+        Serial.println("Error initialising ESP-NOW");
+        return;
     }
-    accErrorX /= 200;
-    accErrorY /= 200;
 
-    for (int i {}; i < 200; ++i) {
+    esp_now_register_send_cb(callbackFunc);
 
-        read_all_imu_gyro();
+    esp_now_peer_info_t peerInfo;
 
-        gyroErrorX += gyroX / 131;
-        gyroErrorY += gyroY / 131;
-        gyroErrorZ += gyroZ / 131;
+    std::move(broadcastAddress.begin(), broadcastAddress.end(), peerInfo.peer_addr);
+    peerInfo.channel = 0;
+    peerInfo.encrypt = false;
 
+    if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+        Serial.println("Failed to add peer");
+        return;
     }
-    gyroErrorX /= 200;
-    gyroErrorY /= 200;
-    gyroErrorZ /= 200;
 
 }
 
+void loop(){
+    sensors_event_t acceleration;
+    sensors_event_t gyro;
+    sensors_event_t temperature;
+    mpu6050.getEvent(&acceleration, &gyro, &temperature);
+    Serial.print(acceleration.acceleration.x);
+    Serial.print("X, ");
+    Serial.print(acceleration.acceleration.y);
+    Serial.print("Y, ");
+    Serial.print(acceleration.acceleration.z);
+    Serial.println("Z");
+
+
+    ImuData data {acceleration.acceleration.x, acceleration.acceleration.y, acceleration.acceleration.z};
+
+    esp_now_send(broadcastAddress.data(), reinterpret_cast<std::uint8_t*>(&data), sizeof(data));
+
+}

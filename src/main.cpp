94faa90f142655c99e8281c43b72b2dc573be8ac
josep <joseph_chiu@outlook.com>
2024-10-27 19:52:14 +0800

    camera code

--- src/main.cpp
+++ src/main.cpp
@@ -1,164 +1,88 @@
-#include <Wire.h>
-#include <Arduino.h>
+#include <WiFi.h>
+#include <esp_now.h>
+#include <queue>
+#include <sstream>
 
-constexpr int MPU = 0x68;
-constexpr int RDX = 0x6b;
-constexpr int ACCEL_CONFIG = 0x1c;
-constexpr int GYRO_CONFIG = 0x1b;
-
-
-double accX {};
-double accY {};
-double accZ {};
-
-double gyroX {};
-double gyroY {};
-double gyroZ {};
-
-double accAngleX {};
-double accAngleY {};
+#define FLASH_GPIO_NUM 4
 
 
-double gyroAngleX {};
-double gyroAngleY {};
 
-double roll {};
-double yaw {};
-double pitch {};
+class ImuData {
+    float accelerationX;
+    float accelerationY;
+    float accelerationZ;
 
-double accErrorX {};
-double accErrorY {};
+    float gyroX;
+    float gyroY;
+    float gyroZ;
 
+    char foot;
+    // to calculate velocity over time -> (t1-t0) * (a1+a0) / 2
+public:
+    ImuData() = default;
 
-double gyroErrorX {};
-double gyroErrorY {};
-double gyroErrorZ {};
+    void printData() const {
 
-double elapsedTime {};
-std::uint64_t currentTime {};
-std::uint64_t previousTime {};
+        std::stringstream ss;
+        ss << "X Acceleration: " << accelerationX << ", Y Acceleration: " << accelerationY << ", Z Acceleration: " << accelerationZ << ", Time elapsed: "
+        << millis() << ", On " << (foot == 'L' ? "Left" : "Right") << "foot";
+        Serial.println(ss.str().c_str());
+    }
 
+    bool isLeftLeg() const {
+        return foot == 'L';
+    }
 
-void calibrate_imu_err();
+};
 
-void read_all_imu_accel();
+static std::queue<ImuData> left_gait_queue_stack {};
+static std::queue<ImuData> right_gait_queue_stack {};
 
-void read_all_imu_gyro();
+void receiveCallback(const uint8_t * mac, const uint8_t *incomingData, int len) {
 
-void setup() {
+    ImuData data {};
+    memcpy(&data, incomingData, sizeof(data));
+    if (data.isLeftLeg()) {
+        left_gait_queue_stack.push(data);
+
+    }
+    else {
+        right_gait_queue_stack.push(data);
+    }
+    digitalWrite(FLASH_GPIO_NUM, HIGH);
+}
+
+void setup(){
+
     Serial.begin(115200);
-    if (!Wire.begin()) {
-        Serial.println("I2C bus cannot be initialised. Check the IMU connection");
+    Serial.println("Gay");
+    pinMode (FLASH_GPIO_NUM, OUTPUT);
+    WiFiClass::mode(WIFI_STA);
+    if (esp_now_init() != ESP_OK) {
+        Serial.println("Error initialising ESP-NOW");
         return;
     }
-    Wire.beginTransmission(MPU);
-    Wire.write(RDX);
-    Wire.write(0);
-    Wire.endTransmission(true);
-
-    Wire.beginTransmission(MPU);
-    Wire.write(ACCEL_CONFIG);
-    Wire.write(0x10);
-    Wire.endTransmission(true);
-
-    Wire.beginTransmission(MPU);
-    Wire.write(GYRO_CONFIG);
-    Wire.write(0x10);
-    Wire.endTransmission(true);
-    delay(20);
-
-    calibrate_imu_err();
-    delay(20);
-
-}
-
-void loop() {
-    read_all_imu_accel();
-
-    accAngleX = std::atan(accY / std::sqrt(std::pow(accX, 2) + std::pow(accZ, 2))) * 180 / PI - 0.58;
-    accAngleY = std::atan(-1 * accX / std::sqrt(std::pow(accY, 2) + std::pow(accZ, 2))) * 180 / PI + 1.58;
-
-    previousTime = currentTime;
-    currentTime = millis();
-
-    elapsedTime = static_cast<double>(currentTime - previousTime) / 1000;
-
-    read_all_imu_gyro();
-
-    gyroX += 0.56;
-    gyroY -= 2;
-    gyroZ += 0.79;
-
-    gyroAngleX += gyroX * elapsedTime;
-    gyroAngleY += gyroY * elapsedTime;
-
-    yaw += gyroZ * elapsedTime;
-    roll = 0.96 * gyroAngleX + 0.04 * accAngleX;
-    pitch = 0.96 * gyroAngleY + 0.04 * accAngleY;
-
-    Serial.print("Acceleration: ");
-    Serial.print(accX);
-    Serial.print(", ");
-    Serial.print(accY);
-    Serial.print(", ");
-    Serial.print(accZ);
-    Serial.println();
-
-    Serial.print("Rotation: ");
-    Serial.print(roll);
-    Serial.print("°, ");
-    Serial.print(pitch);
-    Serial.print("°, ");
-    Serial.print(yaw);
-    Serial.println();
-
-}
-
-void read_all_imu_accel() {
-    Wire.beginTransmission(MPU);
-    Wire.write(0x3b);
-    Wire.endTransmission(true);
-    constexpr std::size_t len {6};
-    Wire.requestFrom(static_cast<std::uint8_t>(MPU), len, true);
-    accX = static_cast<double>(Wire.read() << 8 | Wire.read()) / (2 << 14);
-    accY = static_cast<double>(Wire.read() << 8 | Wire.read()) / (2 << 14);
-    accZ = static_cast<double>(Wire.read() << 8 | Wire.read()) / (2 << 14);
-}
-
-void read_all_imu_gyro() {
-    Wire.beginTransmission(MPU);
-    Wire.write(0x43);
-    Wire.endTransmission(true);
-    constexpr std::size_t len {6};
-    Wire.requestFrom(static_cast<std::uint8_t>(MPU), len, true);
-    gyroX = Wire.read() << 8 | Wire.read();
-    gyroY = Wire.read() << 8 | Wire.read();
-    gyroZ = Wire.read() << 8 | Wire.read();
-}
-
-void calibrate_imu_err() {
-    for (int i {}; i < 200; ++i) {
-        read_all_imu_accel();
-
-        accErrorX += ((std::atan(accY / std::sqrt(std::pow(accX, 2) + std::pow(accZ, 2))) * 180 / PI));
-        accErrorY += ((std::atan(-1 * accY / std::sqrt(std::pow(accY, 2) + std::pow(accZ, 2))) * 180 / PI));
-
+    else {
+        Serial.println("ESP-NOW Initialised");
     }
-    accErrorX /= 200;
-    accErrorY /= 200;
-
-    for (int i {}; i < 200; ++i) {
-
-        read_all_imu_gyro();
-
-        gyroErrorX += gyroX / 131;
-        gyroErrorY += gyroY / 131;
-        gyroErrorZ += gyroZ / 131;
-
-    }
-    gyroErrorX /= 200;
-    gyroErrorY /= 200;
-    gyroErrorZ /= 200;
+    // who the fuck made the part about receiving data a callback with no user context (billions must use global variables)
+    esp_now_register_recv_cb(receiveCallback);
 
 }
 
+void loop(){
+    if (!left_gait_queue_stack.empty()) {
+        left_gait_queue_stack.front().printData();
+        left_gait_queue_stack.pop();
+    }
+    else {
+        Serial.println("No Left Leg Data");
+    }
+    if (!right_gait_queue_stack.empty()) {
+        right_gait_queue_stack.front().printData();
+        right_gait_queue_stack.pop();
+    }
+    else {
+        Serial.println("No Right Leg Data");
+    }
+}
